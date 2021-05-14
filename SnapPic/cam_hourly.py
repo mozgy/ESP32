a4e7232bd77837b8c2b791fc7fd48104e48adaa6
mozgy <mozgy@t-com.hr>
2021-05-15 00:08:14 +0200

    add encode_day

--- SnapPic/cam_hourly.py
+++ SnapPic/cam_hourly.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 
 #
-# v1.03
+# v1.05
 #
 
 import os
@@ -17,9 +17,10 @@ camIP = '192.168.1.66'    # Cam1
 def encode_hour( f_year, f_day, f_hour ):
 
   ## Fix_01?
-  if not os.path.exists( f_day ):
-    os.makedirs( f_day )
-  os.chdir( './{}'.format( f_day ) )
+  pic_dir = '{}{}'.format( f_year, f_day )
+  if not os.path.exists( pic_dir ):
+    os.makedirs( pic_dir )
+  os.chdir( './{}'.format( pic_dir ) )
 
   cmd_call = 'wget --quiet --recursive --no-directories --no-host-directories --timestamping --level=1 --accept="PIC*jpg" "http://{}/ai-cam/{}/{}"'.format( camIP, f_day, f_hour )
   subprocess.call( cmd_call, shell=True )
@@ -27,8 +28,8 @@ def encode_hour( f_year, f_day, f_hour ):
 
   file_list = ''
   aicam_dir = os.getcwd()
-
   sorted_ls = sorted( os.listdir( aicam_dir ) )
+
   for aicam_fn in sorted_ls:
 
     if aicam_fn.startswith( 'PIC-{}{}{}'.format( f_year, f_day, f_hour ) ) and aicam_fn.endswith( 'jpg' ):
@@ -51,11 +52,41 @@ def encode_hour( f_year, f_day, f_hour ):
   os.chdir( '..' )
 
 
+
+def encode_day( f_year, f_day ):
+
+  file_list = ''
+  aicam_dir = os.getcwd()
+  sorted_ls = sorted( os.listdir( aicam_dir ) )
+
+  for aicam_fn in sorted_ls:
+
+    if aicam_fn.startswith( 'cam1-{}'.format( f_day ) ):
+
+      file_list = file_list + "file '{}/{}'\n".format( aicam_dir, aicam_fn )
+      continue
+
+  if file_list != '':
+
+    fd, f_path = tempfile.mkstemp()
+    with os.fdopen( fd, 'w' ) as tmp:
+      tmp.write( file_list )
+
+    cmd_call = 'ffmpeg -f concat -safe 0 -i {} -c copy {}/cam1-{}{}.mkv'.format( f_path, aicam_dir, f_year, f_day )
+    subprocess.call( cmd_call, shell=True )
+    # print( cmd_call )
+
+    os.remove( f_path )
+
+
+
 def fetch_whole_day( f_year, f_day ):
 
   for i_hour in range( 24 ):
     f_hour = '{:02}'.format( i_hour )
     encode_hour( f_year, f_day, f_hour )
+
+  encode_day( f_year, f_day )
 
 
 ### Main Loop
@@ -65,5 +96,6 @@ time_one_hour_ago = time_now - datetime.timedelta( hours = 1 )
 hour = time_one_hour_ago.strftime( "%H" )
 day = time_one_hour_ago.strftime( "%m%d" )
 year = time_one_hour_ago.strftime( "%Y" )
+
 encode_hour( year, day, hour )
 
